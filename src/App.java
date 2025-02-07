eff1508ea2d34adb34caa69029084478978f1ea0
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2025-02-07 12:36:52 +0100

    klar

--- src/App.java
+++ src/App.java
@@ -7,14 +7,14 @@ import java.net.http.HttpResponse.BodyHandlers;
 public class App {
     public static void main(String[] args) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(){
-                .uri(URI.create("http://pokeapi.co/"))
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://pokeapi.co/api/v2/pokemon/snorlax"))
                 .build();
                 client.sendAsync(request, BodyHandlers.ofString())
       .thenApply(HttpResponse::body)
       .thenAccept(System.out::println)
       .join();
 
-    }
+    
 }
 }
